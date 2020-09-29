313e0bc0961c6ff865aaa82acb04e13919ae8f38
civediamo <civediamo@hotmail.com.ar>
2020-09-29 14:21:09 +0200

    rama 1

---
+++ ExerciciPairProgramming/src/com/floristeria/domain/Arboles.java
@@ -0,0 +1,43 @@
+package com.floristeria.domain;
+
+import com.floristeria.excepciones.CampoVacioException;
+
+public class Arboles implements Plantas {
+
+	private String nombre;
+
+	private double altura;
+
+	private double precio;
+
+	public Arboles(String nombre, double altura, double precio) throws CampoVacioException, IllegalArgumentException {
+
+		if (nombre.isEmpty()) {
+
+			throw new CampoVacioException("Has dejado el campo nombre vacio");
+		}
+
+		this.nombre = nombre;
+
+		if (altura < 0 || precio < 0) {
+
+			throw new IllegalArgumentException("Has introducido un número inferior a 0");
+		}
+
+		this.altura = altura;
+
+		this.precio = precio;
+	}
+
+	@Override
+	public String getDescripcion() {
+		// TODO Auto-generated method stub
+		return "Esto es un árbol";
+	}
+
+	@Override
+	public String toString() {
+		return "\t Nombre: " + nombre + ", Altura: " + altura + " cm, Precio: " + precio;
+	}
+
+}
