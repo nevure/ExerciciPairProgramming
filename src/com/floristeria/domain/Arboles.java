c943f932be09b022dfecfb123fc8911bfd8bff72
admin <admin@MacBook-Air-de-admin.local>
2020-10-02 14:58:06 +0200

    update rama

--- src/com/floristeria/domain/Arboles.java
+++ src/com/floristeria/domain/Arboles.java
@@ -10,43 +10,29 @@ public class Arboles implements Plantas {
 
 	private double precio;
 
-	/**
-	 * Constructor
-	 * 
-	 * @param nombre nombre del árbol
-	 * 
-	 * @param altura altura del árbol
-	 * 
-	 * @param precio precio del árbol
-	 */
-
 	public Arboles(String nombre, double altura, double precio) throws CampoVacioException, IllegalArgumentException {
-
-		/**
-		 * @throws Lanzar excepción si el campo está vacío o, si tanto la altura y el
-		 *                precio, están en negativo.
-		 */
 
 		if (nombre.isEmpty()) {
 
 			throw new CampoVacioException("Has dejado el campo nombre vacio");
-		} else if (altura < 0 || precio < 0) {
-
-			throw new IllegalArgumentException("Has introducido un número inferior a 0");
-		} else {
-
-			this.nombre = nombre;
-
-			this.altura = altura;
-
-			this.precio = precio;
 		}
+
+		this.nombre = nombre;
+
+		if (altura < 0 || precio < 0) {
+
+			throw new IllegalArgumentException("Has introducido un nÃºmero inferior a 0");
+		}
+
+		this.altura = altura;
+
+		this.precio = precio;
 	}
 
 	@Override
 	public String getDescripcion() {
 		// TODO Auto-generated method stub
-		return "Esto es un árbol.";
+		return "Esto es un Ã¡rbol";
 	}
 
 	@Override
